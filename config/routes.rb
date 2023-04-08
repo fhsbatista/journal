347e5d41d7e4d9c24f93aedc4181788e87eb47c7
Fernando Batista <fernandohsbatista@gmail.com>
2023-04-07 21:50:19 -0300

    new route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :journal_entries, only: [:index, :create]
+  resources :journal_entries, only: [:index, :create, :show]
   get 'journal_entries/date/:date', to: 'journal_entries#by_date', as: "journal_entries_by_date"
 end
