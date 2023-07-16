9e0e6e3f0e5f0529147f5ee63c3db2bac6fd47dc
Fernando Batista <fernandohsbatista@gmail.com>
2023-07-16 08:45:43 -0300

    generate devise model for User
    command: rails generate devise User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :journal_entries, only: [:index, :create, :show]
   get 'journal_entries/date/:date', to: 'journal_entries#by_date', as: "journal_entries_by_date"
 end
