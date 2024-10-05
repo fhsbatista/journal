c8a87668e9eb77aa5149dd4420689b59bdc8be66
Fernando Batista <fernandohsbatista@gmail.com>
2024-10-05 18:15:54 -0300

    feat:(areas average score in a given date)

--- app/models/area.rb
+++ app/models/area.rb
@@ -8,10 +8,16 @@ class Area
 
   validates :description, presence: true
 
-  def today_score
+  def day_score(date)
     behaviors.includes(:events)
              .flat_map(&:events)
-             .select { |event| event.created_at.to_date == Date.today }
+             .select { |event| event.created_at.to_date == date }
              .sum(&:score)
+  end
+
+  def self.average(date)
+    scores = Area.all.map { |area| area.day_score(date) }
+    average = (scores.sum / Area.count.to_f)
+    average.round(3)
   end
 end
--- spec/models/area_spec.rb
+++ spec/models/area_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe Area, type: :model do
     expect(area.behaviors).to include(behavior1, behavior2)
   end
 
-  describe '#today_score' do
+  describe '#day_score' do
     let!(:area) { Area.create(description: 'Sample Area') }
     let!(:behavior1) { Behavior.create(description: 'Test Behavior', area: area) }
     let!(:behavior2) { Behavior.create(description: 'Test Behavior 2 ', area: area) }
@@ -38,8 +38,46 @@ RSpec.describe Area, type: :model do
       event3.save
     end
 
-    fit 'returns the total score of all today events for all behaviors' do
-      expect(area.today_score).to eq(12.0) # 10.0 + 2.0
+    it 'returns the total score of all today events for all behaviors' do
+      expect(area.day_score(Date.today)).to eq(12.0) # 10.0 + 2.0
+    end
+  end
+
+  describe '#areas day average' do
+    let!(:health) { Area.create(description: 'Health') }
+    let!(:finances) { Area.create(description: 'Finances') }
+    let!(:work) { Area.create(description: 'Work') }
+    let!(:exercises) { Behavior.create(description: 'Exercices', area: health) }
+    let!(:eat_clean) { Behavior.create(description: 'Eat clean', area: health) }
+    let!(:conciliation) { Behavior.create(description: 'Bank accounts conciliation', area: finances) }
+    let!(:pay_bills) { Behavior.create(description: 'Pay all bills', area: finances) }
+    let!(:pomodoros) { Behavior.create(description: '3 pomodoros', area: work) }
+    let!(:score1) { Score.create(score: 2.0, behavior: exercises) }
+    let!(:score2) { Score.create(score: 3.0, behavior: eat_clean) }
+    let!(:score3) { Score.create(score: 2.0, behavior: conciliation) }
+    let!(:score4) { Score.create(score: 1.0, behavior: pay_bills) }
+    let!(:score5) { Score.create(score: 3.0, behavior: pomodoros) }
+    let!(:event1) { Event.create(behavior: exercises, created_at: Date.yesterday) }
+    let!(:event2) { Event.create(behavior: pay_bills, created_at: Date.yesterday) }
+    let!(:event3) { Event.create(behavior: pomodoros, created_at: Date.yesterday) }
+    let!(:event4) { Event.create(behavior: exercises, created_at: Date.today) }
+    let!(:event5) { Event.create(behavior: eat_clean, created_at: Date.today) }
+    let!(:event6) { Event.create(behavior: conciliation, created_at: Date.today) }
+    let!(:event7) { Event.create(behavior: pay_bills, created_at: Date.today) }
+    let!(:event8) { Event.create(behavior: pomodoros, created_at: Date.today) }
+
+    before do
+      event1.save
+      event2.save
+      event3.save
+      event4.save
+      event5.save
+      event6.save
+    end
+
+    it 'returns the total score of all today events for all behaviors' do
+      expect(Area.average(Date.yesterday)).to eq(2)
+      expect(Area.average(Date.today)).to eq(3.667)
     end
   end
 end
